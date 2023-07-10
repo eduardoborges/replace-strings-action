a5584785ae63a456980dac4fe767b5d88582a511
Eduardo Borges <euduardoborges@gmail.com>
2023-07-10 19:31:56 -0300

    another test

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
+import * as core from '@actions/core';
 import fs from 'fs';
 
 const run = () => {
-  const variables = {
-    FIRST_NAME: 'John',
-    LAST_NAME: 'Doe',
-  };
-  let content = fs.readFileSync('./test.txt', 'utf-8');
+  const filePath = core.getInput('file');
+  // Captura todas as variáveis de ambiente
+  const envVariables = process.env;
 
-  Object.entries(variables).forEach(([key, value]) => {
+  // Itera sobre as variáveis de ambiente e as imprime
+  for (const variable in envVariables) {
+    core.info(`${variable}=${envVariables[variable]}`);
+  }
+
+  // Lê o conteúdo do arquivo
+  let content = fs.readFileSync(filePath, 'utf8');
+
+  Object.entries(envVariables).forEach(([key, value]) => {
     const regex = new RegExp(`__${key}__`, 'g');
     content = content.replace(regex, String(value));
   });
 
-  fs.writeFileSync('./test.txt', content, 'utf-8');
+  try {
+    fs.writeFileSync('./foo.txt', content, 'utf-8');
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(content);
+  console.log('Done!');
 };
 
 run();
